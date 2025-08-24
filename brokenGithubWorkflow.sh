745bb7b5f85d3ce78a1ed62d75e9f16e0f33bb97
Abir Khan <118063005+huffpostdata@users.noreply.github.com>
2025-08-24 19:20:50 +0600

    Update brokenGithubWorkflow.sh

--- brokenGithubWorkflow.sh
+++ brokenGithubWorkflow.sh
@@ -1,3 +1,3 @@
 GITHUB_TOKEN=$2
-cat results-github-workflow/$1.txt | xargs -P100 -I {} sh ../bash-scripts/curl/scan-broken-github.sh {} | tee -a results-github-workflow/$1-broken.txt
+cat results-github-workflow/$1.txt | xargs -P100 -I {} sh ../bash-scripts/curl/scan-broken.sh {} | tee -a results-github-workflow/$1-broken.txt
 
