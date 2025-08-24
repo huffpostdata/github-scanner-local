72e967389723fe461a35636235fbbf487109ff32
Abir Khan <118063005+huffpostdata@users.noreply.github.com>
2025-08-24 16:28:09 +0600

    Update githubSearch.sh

--- githubSearch.sh
+++ githubSearch.sh
@@ -1,2 +1,2 @@
 rm -rf results/$1-github-search.txt
-sh github-accounts.sh results/$1-broken.txt | xargs -n1 -P10 -I {} sh search.sh $1 "{}" | tee -a results/$1-github-search.txt
+sh github-accounts.sh results/$1-broken.txt | xargs -P10 -I {} sh search.sh $1 "{}" | tee -a results/$1-github-search.txt
