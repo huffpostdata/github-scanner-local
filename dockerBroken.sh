8fab5ed55affd742e4ee6960a0717a01d143b2ef
Abir Khan <118063005+huffpostdata@users.noreply.github.com>
2025-08-24 16:25:47 +0600

    Update dockerBroken.sh

--- dockerBroken.sh
+++ dockerBroken.sh
@@ -1,4 +1,4 @@
 
-cat docker/$1.txt | xargs -n1 -P5 -I {} sh docker-name.sh {} | tee -a docker/$1-available.txt
+cat docker/$1.txt | xargs -P5 -I {} sh docker-name.sh {} | tee -a docker/$1-available.txt
 
 
