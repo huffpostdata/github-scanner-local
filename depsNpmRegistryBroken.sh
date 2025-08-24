cf1c44ace11c27f626cc890d563d2dfbb81645ca
Abir Khan <118063005+huffpostdata@users.noreply.github.com>
2025-08-24 16:23:50 +0600

    Update depsNpmRegistryBroken.sh

--- depsNpmRegistryBroken.sh
+++ depsNpmRegistryBroken.sh
@@ -1,5 +1,5 @@
-#cat dependencies/npm/$1.txt | awk -F ' ' '{print "https://registry.yarnpkg.com/"$1}' | xargs -n1 -P100 -I {} sh ../bash-scripts/curl/scan-broken.sh {} | tee -a dependencies/npm/$1-broken.txt
+#cat dependencies/npm/$1.txt | awk -F ' ' '{print "https://registry.yarnpkg.com/"$1}' | xargs -P100 -I {} sh ../bash-scripts/curl/scan-broken.sh {} | tee -a dependencies/npm/$1-broken.txt
 
-cat dependencies/npm-registry/$1.txt | awk -F '/' '{print tolower($1)}' | sort | uniq | xargs -n1 -P5 -I {} npm-name {} | tee -a dependencies/npm-registry/$1-available.txt
+cat dependencies/npm-registry/$1.txt | awk -F '/' '{print tolower($1)}' | sort | uniq | xargs -P5 -I {} npm-name {} | tee -a dependencies/npm-registry/$1-available.txt
 
 
